16c7b8d6976e36903f59d49e48473221fcc3fc48
▄︻デS̷a̷n̷s̷k̷a̷r̷ ̷N̷a̷n̷d̷a̷══━一 <88901390+Sanskar-Nanda-11@users.noreply.github.com>
2026-01-27 21:41:50 +0530

    Add Navbar component and integrate into App
    Created a new Navbar component and replaced the default App content with the Navbar. This sets up the structure for future navigation features.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React ,{useEffect}from 'react'
+
+
+const Navbar = () => {
+    
+    const [isopen, setisopen] = useState(false)
+    
+  return (
+  <>
+
+  
+  </>
+  )
+}
+
+export default Navbar
